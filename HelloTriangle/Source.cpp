386245ce7e762220242bb75c86ad9d554f7a214d
Hedo <hedojunior@gmail.com>
2021-04-11 19:09:02 -0300

    adicionando exercicio 3 b

--- HelloTriangle/Source.cpp
+++ HelloTriangle/Source.cpp
@@ -194,17 +194,23 @@ int setupGeometry()
 	// Cada atributo do vértice (coordenada, cores, coordenadas de textura, normal, etc)
 	// Pode ser arazenado em um VBO único ou em VBOs separados
 	GLfloat vertices[] = {
-		-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-		 0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-		 0.0, 0.5, 0.0, 0.0, 0.0, 1.0
+		-0.5, -0.5, 0.0,
+		 0.5, -0.5, 0.0,
+		 0.0, 0.5, 0.0
 	};
 
-	GLuint VBO, VAO;
+	GLfloat vertexColors[] = {
+		 1.0, 0.0, 0.0,
+		 0.0, 1.0, 0.0,
+		 0.0, 0.0, 1.0
+	};
+
+	GLuint verticesVBO, colorsVBO, VAO;
 
 	//Geração do identificador do VBO
-	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &verticesVBO);
 	//Faz a conexão (vincula) do buffer como um buffer de array
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, verticesVBO);
 	//Envia os dados do array de floats para o buffer da OpenGl
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -220,15 +226,21 @@ int setupGeometry()
 	// Se está normalizado (entre zero e um)
 	// Tamanho em bytes 
 	// Deslocamento a partir do byte zero 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
 
 	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
 	// atualmente vinculado - para que depois possamos desvincular com segurança
 	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	glGenBuffers(1, &colorsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, colorsVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColors), vertexColors, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
 	glBindVertexArray(0); 
